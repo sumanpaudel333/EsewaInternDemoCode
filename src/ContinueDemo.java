e6900aefc87f52691f2818c4fb9ae8bfbcf14442
Suman Paudel <sumanpaudel333@gmail.com>
2023-03-22 07:19:25 +0545

    tracking changes

---
+++ src/ContinueDemo.java
@@ -0,0 +1,12 @@
+public class ContinueDemo {
+    public static void main(String[] args){
+        int[] integerArr={10,20,30,40,50};
+        for (int x:integerArr){
+            if(x==30){
+                continue;
+            }
+            System.out.println(x);
+        }
+
+        }
+    }
