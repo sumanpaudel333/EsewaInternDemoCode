393897be285213b6fd9f3f5bce367497d8b445d3
Suman Paudel <sumanpaudel333@gmail.com>
2023-05-23 10:13:35 +0545

    update

---
+++ SpringSecurityDemo/src/main/java/com/project/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -0,0 +1,14 @@
+package com.project.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemoApplication.class, args);
+    }
+
+}
