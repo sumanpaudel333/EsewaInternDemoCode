a0659f1a539579a2716265e81024669a7b93c575
Suman Paudel <sumanpaudel333@gmail.com>
2023-06-01 09:44:22 +0545

    update for adding a common api gateway

--- AddressService/src/main/java/com/microservice/addressservice/controller/AddressController.java
+++ AddressService/src/main/java/com/microservice/addressservice/controller/AddressController.java
@@ -11,20 +11,21 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/address/")
 public class AddressController {
     private final AddressService addressService;
 
-    @GetMapping("/address/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<AddressResponse> getAddressByEmployeeId(@PathVariable("id") int employeeId) {
         AddressResponse address = addressService.getAddressByEmployeeId(employeeId);
         return ResponseEntity.status(HttpStatus.OK).body(address);
     }
-    @GetMapping("/address/getalladdress")
+    @GetMapping("getalladdress")
     public ResponseEntity<List<AddressResponse>> getAllAddress() {
         List<AddressResponse> address = addressService.getAllAddress();
         return ResponseEntity.status(HttpStatus.OK).body(address);
     }
-    @PostMapping("/address/addaddress")
+    @PostMapping("addaddress")
     public String addNewAddress(@RequestBody AddressResponse addressResponse) {
         return addressService.addAddress(addressResponse);
     }
