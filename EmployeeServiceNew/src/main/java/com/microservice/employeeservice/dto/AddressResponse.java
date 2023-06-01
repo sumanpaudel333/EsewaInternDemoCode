b155861840aaee2777864bf86ac88e79c176d90c
Suman Paudel <sumanpaudel333@gmail.com>
2023-06-01 09:45:32 +0545

    update for adding a common api gateway

---
+++ EmployeeServiceNew/src/main/java/com/microservice/employeeservice/dto/AddressResponse.java
@@ -0,0 +1,20 @@
+package com.microservice.employeeservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressResponse implements Serializable {
+    private int id;
+    private String street;
+    private String city;
+    private String state;
+    private int employee_id;
+}
