602981b7fe63f076127a8d1221845c601b1bde7f
Suman Paudel <sumanpaudel333@gmail.com>
2023-06-03 15:30:14 +0545

    Update

--- RatingMicroService/HotelService/src/main/java/com/hotelservice/hotelservice/entity/Hotel.java
+++ RatingMicroService/HotelService/src/main/java/com/hotelservice/hotelservice/entity/Hotel.java
@@ -2,7 +2,7 @@ package com.hotelservice.hotelservice.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,16 +10,17 @@ import lombok.Setter;
 
 import java.util.List;
 
-
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Hotel {
-
+    @Id
     private String hotelId;
     private String name;
     private String location;
     private String about;
+    @Transient
     private List<Rating> ratings;
 }
