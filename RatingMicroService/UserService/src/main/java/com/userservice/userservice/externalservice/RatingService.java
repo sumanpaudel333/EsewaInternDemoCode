3a66f35ef4f9eafdb0ba12413e793cbad8b8d9f5
Suman Paudel <sumanpaudel333@gmail.com>
2023-06-05 10:03:56 +0545

    Update

--- RatingMicroService/UserService/src/main/java/com/userservice/userservice/externalservice/RatingService.java
+++ RatingMicroService/UserService/src/main/java/com/userservice/userservice/externalservice/RatingService.java
@@ -2,14 +2,18 @@ package com.userservice.userservice.externalservice;
 
 import com.userservice.userservice.entity.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "RATING-SERVICE",path = "/api/rating")
+@FeignClient(name = "RATING-SERVICE", path = "/api/rating")
 public interface RatingService {
     @GetMapping("/getratingbyuser/{userId}")
-   List<Rating> listOfRatingByUserId(@PathVariable String userId);
+    List<Rating> listOfRatingByUserId(@PathVariable String userId);
+
+    @PostMapping("/addrating")
+    Rating addRating(@RequestBody Rating rating);
 }
