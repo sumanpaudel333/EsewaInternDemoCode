3135a399fc758d85f0eafd0ddf3dc8487306a06f
Suman Paudel <sumanpaudel333@gmail.com>
2023-07-03 11:29:25 +0545

    Router and Handler Configuration

---
+++ ReactiveProgrammingDemo/src/main/java/com/springwebfluxdemo/reactiveprogrammingdemo/handler/CustomerHandler.java
@@ -0,0 +1,29 @@
+package com.springwebfluxdemo.reactiveprogrammingdemo.handler;
+
+import com.springwebfluxdemo.reactiveprogrammingdemo.dao.CustomerDao;
+import com.springwebfluxdemo.reactiveprogrammingdemo.dto.Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerHandler {
+    private final CustomerDao customerDao;
+    public Mono<ServerResponse> loadCustomers(ServerRequest serverRequest){
+        Flux<Customer> customerList=customerDao.getCustomerList();
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(customerList, Customer.class);
+    }
+    public Mono<ServerResponse> loadAllCustomers(ServerRequest serverRequest){
+        Flux<Customer> customerList=customerDao.getCustomerStream();
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(customerList, Customer.class);
+    }
+}
---
+++ ReactiveProgrammingDemo/src/main/java/com/springwebfluxdemo/reactiveprogrammingdemo/router/RouterConfig.java
@@ -0,0 +1,21 @@
+package com.springwebfluxdemo.reactiveprogrammingdemo.router;
+
+import com.springwebfluxdemo.reactiveprogrammingdemo.handler.CustomerHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+@RequiredArgsConstructor
+public class RouterConfig {
+    private final CustomerHandler customerHandler;
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction(){
+        return RouterFunctions.route()
+                .GET("/router/customers",customerHandler::loadAllCustomers)
+                .GET("/router/customers/stream",customerHandler::loadCustomers).build();
+    }
+}
